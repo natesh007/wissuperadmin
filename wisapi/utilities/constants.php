596ef206814a095701e90e5ebd7ab24d8558ff75
Aparna T <aparna@igreensystems.com>
2022-06-25 17:58:46 +0530

    Api related files

---
+++ wisapi/utilities/constants.php
@@ -0,0 +1,15 @@
+<?php
+//Local Settings
+ /*define("DBHOST","LOCALHOST");
+ define("DBUSER","root");
+ define("DBPASS","");
+ define("DBNAME","soni");*/
+
+//Server Settings
+define("DBHOST","LOCALHOST");
+define("DBUSER","lamaison_dev");
+define("DBPASS","Traceout007");
+define("DBNAME","lamaison_wis");
+
+define("SITE_URL","");
+?>
