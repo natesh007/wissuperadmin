89ff2748188a356c6109923b7273c602744efc60
Lakshmi-IGS <lakshmi@igreensystems.com>
2022-07-14 19:33:08 +0530

    Building And Floors And Rooms Page Design

--- wis/Admin/Views/building_form.php
+++ wis/Admin/Views/building_form.php
@@ -15,10 +15,10 @@
 			<section class="content">
 				<div class="container-fluid">
 					<!-- Main row -->
-					<div class="card">
-						<div class="card-body">
-							<div class="row">
-								<form class="form-horizontal" method="post" action="<?= base_url('admin/buildings/add_building') ?>" style="width:100%" id="add_building" method="post">
+					<div class="row">
+						<form class="form-horizontal" method="post" action="<?= base_url('admin/buildings/add_building') ?>" style="width:100%" id="add_building" method="post">
+							<div class="card">
+								<div class="card-body">		
 									<div class="form-group row">
 										<div class="col-md-4">
 											<label for="OrgID">Organization<strong class="help-block">*</strong></label>
@@ -29,7 +29,6 @@
 												} ?>
 											</select>
 										</div>
-
 										<div class="col-md-4">
                                             <label for="BrID">Branch</label>
                                             <select class="form-control" name="BrID" id="BrID">
@@ -43,41 +42,38 @@
 											<span id="name_error"></span>
 										</div>
 									</div>
-									
-
-									<hr>
-									<h4>Floor Info</h4>
-									<div class="form-group row">
-										<div class="col-md-4">
-											<div id="inputFormRow">
-											<div class="input-group mb-3">
-												<input type="text" name="FloorName[]" class="form-control m-input" placeholder="Enter Floo rName" autocomplete="off">
-												<div class="input-group-append">
-												
-												</div>
-											</div>
-											</div>
-										</div>
-										<div class="col-md-2">
-											<button id="addRow" type="button" class="btn btn-sm btn-info">+ Add Floor</button>
-										</div>
-										
-									</div>
-
-									<div class="form-group row">
-										<div class="col-md-4" >
-										<div id="newRow"></div>
-											</div>
-									</div>
-
-									
-									<div class="form-group text-center">
-										<button type="submit" id="submit" name="submit" class="btn btn-sm btn-success">Save</button>
-										<a data-toggle="tooltip" title="Cancel" href="<?= base_url(); ?>/admin<?= session()->get('building_page'); ?>" class="btn btn-sm btn-primary">Cancel</a>
-									</div>
-								</form>
+								</div>
 							</div>
-						</div>
+							<h4>Floors & Rooms Info</h4>
+							<div class="card">
+								<div class="card-body">
+									<div class="MainFloor">
+										<div class="form-group row">
+											<div class="col-md-4">
+												<input type="text" name="FloorName[]" class="form-control" placeholder="Enter Floor Name" autocomplete="off">
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-3 mb-2 MainRoom1 d-flex">
+												<input type="text" name="RoomName[]" class="form-control RoomName" placeholder="Enter Room Name" autocomplete="off"/>
+											</div>
+											<div class="col-md-1 my-auto">
+												<button type="button" class="btn btn-sm btn-success" onclick="AddMoreRooms(1)"><span class="fa fa-plus"></span> Add Room</button>
+											</div>
+										</div>
+										<hr>
+									</div>
+									<div class="col-md-12 text-right">
+										<button type="button" class="btn btn-sm btn-success" id="AddMoreFloorsBtn" onclick="AddMoreFloors(2)"><span class="fa fa-plus"></span> Add Floor</button>
+										&nbsp;
+									</div>
+								</div>
+							</div>
+							<div class="form-group text-center">
+								<button type="submit" id="submit" name="submit" class="btn btn-sm btn-success">Save</button>
+								<a data-toggle="tooltip" title="Cancel" href="<?= base_url(); ?>/admin<?= session()->get('building_page'); ?>" class="btn btn-sm btn-primary">Cancel</a>
+							</div>
+						</form>
 					</div>
 					<!-- /.row (main row) -->
 				</div><!-- /.container-fluid -->
@@ -128,25 +124,22 @@
                 }
             });
 
-			// add row
-			$("#addRow").click(function () {
-				var html = '';
-				html += '<div id="inputFormRow">';
-				html += '<div class="input-group mb-12">';
-				html += '<input type="text" name="FloorName[]" class="form-control m-input" placeholder="Enter FloorName" autocomplete="off">';
-				html += '<div class="input-group-append">';
-				html += '<button id="removeRow" type="button" class="btn btn-danger">X</button>';
-				html += '</div>';
-				html += '</div>';
-
-				$('#newRow').append(html);
-			});
-
+			// Add More Rooms
+			function AddMoreRooms(num){
+				$('<div class="col-md-3 mb-2 MainRoom'+num+' d-flex RoomeBlk"><input type="text" name="RoomName[]" class="form-control RoomName" placeholder="Enter Room Name" autocomplete="off" style="width: 90%;"/><button type="button" class="btn btn-sm btn-danger RemoveRoom"><span class="fa fa-minus"></span></button></div>').insertAfter($('.MainRoom'+num+'').last());
+			}
+			// Add More Floors
+			function AddMoreFloors(num){
+				$('<div class="MainFloor"><div class="form-group row"><div class="col-md-4"><input type="text" name="FloorName[]" class="form-control" placeholder="Enter Floor Name" autocomplete="off"></div></div><div class="row"><div class="col-md-3 mb-2 MainRoom'+num+'"><input type="text" name="RoomName[]" class="form-control" placeholder="Enter Room Name" autocomplete="off"/></div><div class="col-md-1 my-auto"><button type="button" class="btn btn-sm btn-success" onclick="AddMoreRooms('+num+')"><span class="fa fa-plus"></span> Add Room</button></div></div><div class="col-md-12 text-right"><button type="button" class="btn btn-sm btn-danger RemoveFloor"><span class="fa fa-minus"></span> Remove Floor</button></div><hr></div>').insertAfter($('.MainFloor').last());
+				$("#AddMoreFloorsBtn").attr("onclick", "AddMoreFloors("+(num+1)+")");
+			}
 			// remove row
-			$(document).on('click', '#removeRow', function () {
-				$(this).closest('#inputFormRow').remove();
+			$(document).on('click', '.RemoveRoom', function () {	
+				$(this).closest('div.RoomeBlk').remove();
 			});
-		
+			$(document).on('click', '.RemoveFloor', function () {	
+				$(this).closest('div.MainFloor').remove();
+			});
 		</script>
 	</body>
 </html>
